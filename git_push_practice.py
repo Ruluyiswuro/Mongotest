7edce7006307a9213efb218f160b8a7cef48761a
Ruluyi <ruluyiswuro@gmail.com>
2022-07-19 17:28:05 +0530

    Practice push

---
+++ git_push_practice.py
@@ -0,0 +1,10 @@
+s = "rulu"
+n = 56
+print(s[2])
+l = [2,3,4,5,5, "rulu", 24.34,34.54, 6+7j, [2,3,4,4]]
+t = (2,2,3,53)
+#d = {"y":"fsds", 4:"taf", n{"name": "rulu", "email":"rulu@gmail.com"}}
+print(s[0])
+print(s[0:100])
+for i in s:
+    print(i)
